976e06943aeda2bf9ff925dc031126c4aed05938
Tomke <pfoch.tomke@gmail.com>
2025-01-24 03:48:24 +0100

    internal type handeling

--- List.h
+++ List.h
@@ -5,45 +5,71 @@
 // Basic generic Linked List
 
 #include "stdlib.h"
+#include <stddef.h>
 
-#define _LIST_INC(T, ALIAS,...)                                      \
-        struct List_##ALIAS;                                         \
-        typedef struct List_##ALIAS* List_##ALIAS;                   \
-        T* List_##ALIAS##_next(struct List_##ALIAS ** const node);   \
-        T* List_##ALIAS##_insert(struct List_##ALIAS **node, T val); \
-        T List_##ALIAS##_pop(struct List_##ALIAS **node);
+#define _LIST_DECL(T, ALIAS,...)                                          \
+struct List_##ALIAS;                                                      \
+typedef T _List_##ALIAS##_T;                                              \
+typedef struct List_##ALIAS* List_##ALIAS;                                \
+_List_##ALIAS##_T*                                                        \
+List_##ALIAS##_next(struct List_##ALIAS ** const node);                   \
+_List_##ALIAS##_T*                                                        \
+List_##ALIAS##_push(struct List_##ALIAS **node, _List_##ALIAS##_T val);   \
+_List_##ALIAS##_T*                                                        \
+List_##ALIAS##_insert(struct List_##ALIAS **node, _List_##ALIAS##_T val); \
+_List_##ALIAS##_T List_##ALIAS##_pop(struct List_##ALIAS **node);         \
+size_t List_##ALIAS##_length(struct List_##ALIAS ** const node);
 
-#define _LIST_IMPL(T,ALIAS, ...)                                       \
-struct List_##ALIAS                                                    \
-{                                                                      \
-        T val;                                                         \
-        struct List_##ALIAS *next;                                     \
-};                                                                     \
-T* List_##ALIAS##_next(struct List_##ALIAS ** const node)              \
-{                                                                      \
-        if(!*node) return NULL;                                             \
-        T* res = &(*node)->val;                                        \
-        *node = (*node)->next;                                         \
-        return res;                                                         \
-}                                                                      \
-T* List_##ALIAS##_insert(struct List_##ALIAS **node, T val)            \
-{                                                                      \
-        struct List_##ALIAS* res = malloc(sizeof(struct List_##ALIAS));\
-        if(!res) return NULL;                                              \
-        *res = (struct List_##ALIAS){val, *node};                      \
-        *node = res;                                                   \
-        return &res->val;                                                   \
-}                                                                      \
-T List_##ALIAS##_pop(struct List_##ALIAS** node)                       \
-{                                                                      \
-        T res = (*node)->val;                                          \
-        struct List_##ALIAS* next = (*node)->next;                     \
-        free(*node);                                                   \
-        *node = next;                                                  \
-        return res;                                                         \
+#define LIST_IMPL(ALIAS)                                                 \
+struct List_##ALIAS                                                      \
+{                                                                        \
+        _List_##ALIAS##_T val;                                           \
+        struct List_##ALIAS *next;                                       \
+};                                                                       \
+_List_##ALIAS##_T*                                                       \
+List_##ALIAS##_next(struct List_##ALIAS ** const node)                   \
+{                                                                        \
+        if(!*node) return NULL;                                               \
+        _List_##ALIAS##_T* res = &(*node)->val;                          \
+        *node = (*node)->next;                                           \
+        return res;                                                           \
+}                                                                        \
+_List_##ALIAS##_T*                                                       \
+List_##ALIAS##_push(struct List_##ALIAS **node, _List_##ALIAS##_T val)   \
+{                                                                        \
+        struct List_##ALIAS* res = malloc(sizeof(struct List_##ALIAS));  \
+        if(!res) return NULL;                                                 \
+        *res = (struct List_##ALIAS){val, *node};                        \
+        *node = res;                                                     \
+        return &res->val;                                                     \
+}                                                                        \
+_List_##ALIAS##_T*                                                       \
+List_##ALIAS##_insert(struct List_##ALIAS **node, _List_##ALIAS##_T val) \
+{                                                                        \
+        struct List_##ALIAS* res = malloc(sizeof(struct List_##ALIAS));  \
+        if(!res) return NULL;                                                 \
+        while(*node) node = &(*node)->next;                              \
+        *res = (struct List_##ALIAS){val, NULL};                         \
+        *node = res;                                                     \
+        return &res->val;                                                     \
+}                                                                        \
+_List_##ALIAS##_T                                                        \
+List_##ALIAS##_pop(struct List_##ALIAS** node)                           \
+{                                                                        \
+        _List_##ALIAS##_T res = (*node)->val;                            \
+        struct List_##ALIAS* next = (*node)->next;                       \
+        free(*node);                                                     \
+        *node = next;                                                    \
+        return res;                                                           \
+}                                                                        \
+size_t List_##ALIAS##_length(struct List_##ALIAS ** const node)          \
+{                                                                        \
+        size_t res = 0;                                                  \
+        List_##ALIAS iter = *node;                                       \
+        while(List_##ALIAS##_next(&iter)) res++;                         \
+        return res;                                                           \
 }
-#define LIST_IMPL(...) _LIST_IMPL(__VA_ARGS__, __VA_ARGS__,)
-#define LIST_INC(...) _LIST_INC(__VA_ARGS__, __VA_ARGS__,)
+#define LIST_DECL(...) _LIST_DECL(__VA_ARGS__, __VA_ARGS__,)
 
 
 #endif // LIST_H_
--- Map.h
+++ Map.h
@@ -1,41 +1,47 @@
 #ifndef MAP_H_
 #define MAP_H_
 
-#include "List.h"
-
 // Extend List with Map operations
 
-#define MAP_INC(K, T, ALIAS)                                            \
-    _LIST_INC(T,ALIAS,)                                                 \
-    struct List_##ALIAS** Map_##ALIAS##_find(struct List_##ALIAS **current_ptr, K key); \
-    T* Map_##ALIAS##_get(struct List_##ALIAS ** const current_ptr, K key);     \
-    int Map_##ALIAS##_remove(struct List_##ALIAS **current_ptr, K key, T *out);
+#define MAP_DECL(K, ALIAS)                                 \
+typedef K _Map_##ALIAS##_K;                                \
+List_##ALIAS* Map_##ALIAS##_find(List_##ALIAS *current_ptr,\
+                                 _Map_##ALIAS##_K key);    \
+_List_##ALIAS##_T*                                         \
+Map_##ALIAS##_get(List_##ALIAS *const current_ptr,         \
+                  _Map_##ALIAS##_K key);                   \
+int Map_##ALIAS##_remove(List_##ALIAS *current_ptr,          \
+                       _Map_##ALIAS##_K key,               \
+                       _List_##ALIAS##_T *out);
 
-#define MAP_IMPL(K, T, ALIAS, CMP)                                                        \
-        _LIST_IMPL(T,ALIAS, )                                            \
-    struct List_##ALIAS ** Map_##ALIAS##_find(struct List_##ALIAS **current_ptr, K key){ \
-    struct List_##ALIAS *current = *current_ptr;                                          \
-        while(current){                                                                   \
-            if(CMP(key, &current->val))                                                   \
-                return current_ptr;                                                            \
-            current_ptr = &current->next;                                                 \
-            current = current->next;                                                      \
-        }                                                                                 \
-        return NULL;                                                                           \
-    }                                                                                     \
-    T* Map_##ALIAS##_get(struct List_##ALIAS ** const map, K key) {                                     \
-        struct List_##ALIAS **node = Map_##ALIAS##_find(map, key);                               \
-        if(!node) return NULL;                                                                \
-        return &(*node)->val;                                                                  \
-    }                                                                                     \
-    int Map_##ALIAS##_remove(struct List_##ALIAS **map, K key, T* out){                    \
-        struct List_##ALIAS **node = Map_##ALIAS##_find(map, key);                       \
-        if(!node) return -1;                                                                  \
-        if(out)                                                                           \
-            *out = List_##ALIAS##_pop(node);                                              \
-        else                                                                              \
-            List_##ALIAS##_pop(node);                                                     \
-        return 0;                                                                              \
-    }
+#define MAP_IMPL(ALIAS, CMP)                                         \
+List_##ALIAS *                                                       \
+Map_##ALIAS##_find(List_##ALIAS *current_ptr, _Map_##ALIAS##_K key){ \
+struct List_##ALIAS *current = *current_ptr;                         \
+    while(current){                                                  \
+        if(CMP(key, &current->val))                                  \
+            return current_ptr;                                           \
+        current_ptr = &current->next;                                \
+        current = current->next;                                     \
+    }                                                                \
+    return NULL;                                                          \
+}                                                                    \
+_List_##ALIAS##_T*                                                   \
+Map_##ALIAS##_get(List_##ALIAS *const map, _Map_##ALIAS##_K key) {   \
+    List_##ALIAS *node = Map_##ALIAS##_find(map, key);               \
+    if(!node) return NULL;                                               \
+    return &(*node)->val;                                                 \
+}                                                                    \
+int Map_##ALIAS##_remove(List_##ALIAS *map,                            \
+                       _Map_##ALIAS##_K key,                         \
+                       _List_##ALIAS##_T* out){                      \
+    List_##ALIAS *node = Map_##ALIAS##_find(map, key);               \
+    if(!node) return -1;                                                 \
+    if(out)                                                          \
+        *out = List_##ALIAS##_pop(node);                             \
+    else                                                             \
+        List_##ALIAS##_pop(node);                                    \
+    return 0;                                                             \
+}
 
 #endif // MAP_H_
