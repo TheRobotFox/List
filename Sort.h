4057aac9b85473c95252d98d24f08a50ad5ff852
Tomke <pfoch.tomke@gmail.com>
2025-01-24 03:48:16 +0100

    sort

---
+++ Sort.h
@@ -0,0 +1,53 @@
+#ifndef SORT_H_
+#define SORT_H_
+
+#include <stddef.h>
+#define SORT_DECL(ALIAS)                            \
+typedef int (*_Sort_##ALIAS##_f)(const _List_##ALIAS##_T *a,\
+                               const _List_##ALIAS##_T *b);\
+void List_##ALIAS##_sort(List_##ALIAS *node, _Sort_##ALIAS##_f cmp);
+
+#define SORT_IMPL(ALIAS)                                                \
+    void _List_##ALIAS##_merge_sort(List_##ALIAS *node,                 \
+                                    size_t length,                      \
+                                    _Sort_##ALIAS##_f cmp)              \
+{                                                                       \
+    if(length<2){                                                       \
+        (*node)->next = NULL;                                           \
+        return;                                                         \
+    }                                                                   \
+    List_##ALIAS a = *node, b, *ptr = node;                             \
+    size_t step = length/2;                                             \
+    while(step--) ptr=&(*ptr)->next;                                    \
+    b=*ptr;                                                             \
+    _List_##ALIAS##_merge_sort(&a, length/2, cmp);                      \
+    _List_##ALIAS##_merge_sort(&b, length-(length/2), cmp);             \
+                                                                        \
+    while( a&&b ){                                                       \
+        if(cmp(&a->val, &b->val)){                                      \
+            *node = a;                                                  \
+            node = &a->next;                                            \
+            a = a->next;                                                \
+        }else{                                                          \
+            *node = b;                                                  \
+            node = &b->next;                                            \
+            b = b->next;                                                \
+        }                                                               \
+    }                                                                   \
+    while(a){                                                           \
+        *node = a;                                                      \
+        node = &a->next;                                                \
+        a = a->next;                                                    \
+    }                                                                   \
+    while(b){                                                           \
+        *node = b;                                                      \
+        node = &b->next;                                                \
+        b = b->next;                                                    \
+    }                                                                   \
+}                                                                       \
+void List_##ALIAS##_sort(List_##ALIAS *node, _Sort_##ALIAS##_f cmp)     \
+{                                                                       \
+_List_##ALIAS##_merge_sort(node, List_##ALIAS##_length(node), cmp);     \
+}
+
+#endif // SORT_H_
